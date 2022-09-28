2df568b820b4ac0f4e8ee85823175a6d82f0381e
dirumahrafif <dirumahrafif@gmail.com>
2022-09-28 11:40:37 +0700

    first commit

---
+++ database/migrations/2022_09_27_223458_create_mahasiswas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->integer('nim');
+            $table->unique('nim');
+            $table->string('nama');
+            $table->string('jurusan');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswa');
+    }
+};
